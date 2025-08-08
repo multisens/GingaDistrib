19ff2f9475dcf8e6d325097fe353384a7b8b64ba
Joel dos Santos <jsantos@eic.cefet-rj.br>
2025-08-08 15:30:33 -0300

    Apps: adjusting stream parameters

--- apps/stream.sh
+++ apps/stream.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
 path=$(dirname $1)
 file=$(basename $1)
-stream_out="${path}/stream/${file%.*}.m3u8"
+if [ -n "$2" ]; then
+    stream_name=${2%.*}
+else
+    stream_name=${file%.*}
+fi
+stream_out="${path}/stream/${stream_name}.m3u8"
+segment_out="${path}/stream/${file%.*}_%03d.ts"
 
 mkdir -p "${path}/stream"
+echo -e "\n\n-------------------------------------------------------------------"
 echo "Streaming video ${file} into stream ${stream_out}"
+echo -e "-------------------------------------------------------------------\n\n"
 
 # FFmpeg options:
 # -re                           : reads the video file in real time
-# -stream_loop -1               : repeats the video in loop
 # -i $1                         : input video file
-# -c:v libx264                  : output video format h.264
+# -c:v libx264 -g ...           : output video format h.264, other flags force keyframes to respect the segment size
 # -c:a aac                      : output audio format aac
 # -f hls                        : output stream in HLS format
-# -hls_time 2                   : HLS file segment with 2 seconds
-# -hls_list_size 5              : Keep 5 segments in the playlist
+# -hls_time 1                   : HLS file segment with 1 seconds
+# -hls_list_size 2              : Keep 2 segments in the playlist
 # -hls_flags delete_segments    : Remove old segments
 # -hls_flags append_list        : Live list update
 # -hls_flags omit_endlist       : Never end the stream
+# -fflags +nobuffer             : deactivate the in/out buffer for faster stream
 ffmpeg  -re \
-        -stream_loop -1 \
         -i $1 \
-        -c:v libx264 \
+        -c:v libx264 -g 30 -keyint_min 30 -force_key_frames "expr:gte(n, n_forced * 30)" \
         -c:a aac \
         -f hls \
-        -hls_time 2 \
-        -hls_list_size 5 \
+        -hls_segment_filename $segment_out \
+        -hls_time 1 \
+        -hls_list_size 2 \
         -hls_flags delete_segments+append_list+omit_endlist \
+        -fflags +nobuffer \
         $stream_out
 
-# echo "Streaming ended, cleaning output files"
-# rm "${path}/stream/*"
+echo -e "\n\n-------------------------------------------------------------------"
+echo "Streaming ended, cleaning output files"
+echo -e "-------------------------------------------------------------------\n\n"
+rm ${path}/stream/*

7c52ea96aa0b8acea1770b0282ef6b2eb6459512
Rodrigo Mota <rmosodre@gmail.com>
2025-08-13 20:16:39 -0300

    Changes for SetExpo

---
+++ setexpo.config.js
@@ -0,0 +1,71 @@
+module.exports = {
+    apps: [
+        {
+            name: 'broker',
+            script: '/usr/sbin/mosquitto',
+            args: '-c /home/dell-g15-1/Documentos/SetExpo/GingaDistrib/mosquitto/mosquitto.conf',
+            interpreter: 'none',
+            wait_ready: true,
+            autorestart: false
+        },
+        {
+            name: 'aop',
+            cwd: './aop',
+            script: 'npm',
+            args: 'start',
+            interpreter: 'none',
+            wait_ready: true,
+            autorestart: false,
+            env: {
+                PORT: 8080,
+                BROKER: 'mqtt://localhost',
+                SCREENWIDTH: 1440,
+                USER_DATA_PATH: '../user-files'
+            }
+        },
+        {
+            name: 'apps',
+            cwd: './apps',
+            script: 'npm',
+            args: 'start',
+            interpreter: 'none',
+            wait_ready: true,
+            autorestart: false,
+            env: {
+                PORT: 8081,
+                HOST_IP: '10.100.20.70',
+                BROKER: 'mqtt://localhost',
+                USER_DATA_PATH: '../user-files'
+            }
+        },
+        {
+            name: 'tv3ws',
+            cwd: './ccws',
+            script: 'npm',
+            args: 'start',
+            interpreter: 'none',
+            wait_ready: true,
+            env: {
+                PORT: 44642,
+                BROKER: 'mqtt://localhost',
+                SERVER_URL: 'localhost',
+                USER_DATA_FILE: '../user-files/userData.json',
+                USER_THUMBS: '../user-files/thumbs'
+            }
+        },
+        {
+            name: "chrome-topic-explorer",
+            interpreter: "/bin/bash",
+            autorestart: false,
+            script: "./start-chrome.sh",
+            args: "./mqtt-explorer/index.html",
+        },
+        {
+            name: "chrome-aop",
+            interpreter: "/bin/bash",
+            autorestart: false,
+            script: "./start-chrome.sh",
+            args: "http://localhost:8080",
+        },
+    ]
+}

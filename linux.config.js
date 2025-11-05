f94a6cdc48e564469cdd0b90c32bb77d540ce113
Rodrigo Sodré <rmsodre@id.uff.br>
2025-11-05 17:23:47 -0300

    Updated linux pm2 config to new TV30 submodules structure

--- linux.config.js
+++ linux.config.js
@@ -18,24 +18,8 @@ module.exports = {
             autorestart : false,
             env : {
                 PORT            : 8080,
-                BROKER          : 'mqtt://localhost',
-                SCREENWIDTH     : 1440,
+                MQTT_HOST          : 'localhost',
                 USER_DATA_PATH  : '../user-files'
-            }
-        },
-        {
-            name: 'apps',
-            cwd: './apps',
-            interpreter: "none",
-            script: "/bin/bash",
-            args: '-c "npm run build && npm start"',
-            wait_ready: true,
-            autorestart: false,
-            env: {
-                PORT: 8081,
-                HOST_IP: '192.168.1.21',
-                BROKER: 'mqtt://localhost',
-                USER_DATA_PATH: '../user-files'
             }
         },
         {
@@ -52,36 +36,6 @@ module.exports = {
                 USER_DATA_FILE: '../user-files/userData.json',
                 USER_THUMBS: '../user-files/thumbs'
             }
-        },
-        {
-            name: "chrome-topic-explorer",
-            interpreter: "/bin/bash",
-            autorestart: false,
-            script: "./start-chrome.sh",
-            args: "./mqtt-explorer/index.html",
-        },
-        {
-            name: "chrome-aop",
-            interpreter: "/bin/bash",
-            autorestart: false,
-            script: "./start-chrome.sh",
-            args: "http://localhost:8080",
-        },
-        {
-            name: "eduplay",
-            cwd: "./eduplay",
-            interpreter: "none",
-            script: "/bin/bash",
-            args: '-c "npm run build && npm start"',
-            wait_ready: true,
-        },
-        {
-            name: "sepe",
-            cwd: "./sepe",
-            interpreter: "none",
-            script: "/bin/bash",
-            args: '-c "npm run build && npm start"',
-            wait_ready: true,
         }
     ]
 }
